ad66d05afefcd43084a0b83c331b0d6e4ee9f7f7
Lucavinini <lucas.00000850855@unicap.br>
2026-09-04 19:49:00 -0300

    feat(trees): implementa operações

---
+++ trees/arvore.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arvore.h"
+
+struct arv {
+    char info;
+    Arv *sae;
+    Arv *sad;
+};
+
+Arv *arv_criavazia()
+{
+    return NULL;
+}
+
+Arv *arv_cria(char c, Arv *sae, Arv *sad)
+{
+    Arv *a = malloc(sizeof(Arv));
+
+    if (a == NULL) {
+        return NULL;
+    }
+
+    a->info = c;
+    a->sae = sae;
+    a->sad = sad;
+
+    return a;
+}
+
+int arv_vazia(Arv *a)
+{
+    return a == NULL;
+}
+
+int arv_pertence(Arv *a, char c)
+{
+    if (arv_vazia(a)) {
+        return 0;
+    }
+
+    return a->info == c || arv_pertence(a->sae, c) || arv_pertence(a->sad, c);
+}
+
+Arv *arv_libera(Arv *a)
+{
+    if (!arv_vazia(a)) {
+        arv_libera(a->sae);
+        arv_libera(a->sad);
+        free(a);
+    }
+
+    return NULL;
+}
+
+void arv_imprime(Arv *a)
+{
+    if (!arv_vazia(a)) {
+        printf("<%c", a->info);
+        arv_imprime(a->sae);
+        arv_imprime(a->sad);
+        printf(">");
+    }
+}
